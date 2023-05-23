30841b1653da44311921e474a37b13688b4a439f
gabewolford <gabewolford@gmail.com>
2023-05-23 11:07:04 -0700

    added ScrollToTop component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,13 +3,15 @@ import Nav from "../Nav"
 import HomePage from "../HomePage"
 import WorkPage from "../WorkPage"
 import MePage from "../MePage"
+import ScrollToTop from "../ScrollToTop"
 
 function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Nav />
-
+      
       <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/work" element={<WorkPage />} />
