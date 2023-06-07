1e3b63b2f67019a54ee106ac5cf93657a7a5ffcc
gabewolford <gabewolford@gmail.com>
2023-06-06 17:19:09 -0700

    added z index to nav component

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Nav() {
     return (
         <>
-        <nav className="fixed">
+        <nav className="fixed z-50">
             <Link 
                 to='/'
                 className="fixed text-sm top-8 left-8 px-2 py-1 bg-transparent rounded lg:top-8 lg:left-8 hover:text-pink hover:underline hover:underline-offset-8 hover:decoration-2 hover:font-bold transtion ease-in-out duration-500"
